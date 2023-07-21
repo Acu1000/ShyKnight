0ce93f699fde96c155c5acd9c715145b13237e51
Acu1000 <140196544+Acu1000@users.noreply.github.com>
2023-07-22 01:37:14 +0200

    Initial commit

---
+++ ModClass.cs
@@ -0,0 +1,54 @@
+﻿using Modding;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UObject = UnityEngine.Object;
+
+namespace ShyKnight
+{
+    public class ShyKnight : Mod
+    {
+        internal static ShyKnight Instance;
+
+        public override string GetVersion() => "v1.2.0";
+
+        public ShyKnight() : base("ShyKnight")
+        {
+            Instance = this;
+        }
+
+        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
+        {
+            Instance = this;
+            On.GameManager.OnNextLevelReady += OnSceneLoad;
+        }
+
+        public void OnSceneLoad(On.GameManager.orig_OnNextLevelReady orig, global::GameManager self)
+        {
+            orig(self);
+            RemoveAudience();
+        }
+
+        private void RemoveAudience()
+        {
+            // Check if the current scene contains godhome audience
+            GameObject arena_prefab = GameObject.Find("GG_Arena_Prefab");
+            if (arena_prefab != null)
+            {
+                // Remove the audience
+                GameObject crowd = GameObject.Find("Crowd");
+                if (crowd != null)
+                {
+                    crowd.SetActive(false);
+                }
+                // Remove audience Godseeker
+                GameObject gs_crowd = GameObject.Find("Godseeker Crowd");
+                if (gs_crowd != null)
+                {
+                    gs_crowd.SetActive(false);
+                }
+            }
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# ShyKnight
+
+A mod that removes the animated background audience from godhome arenas. Makes it easier to focus on the foreground.
